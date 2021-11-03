ddedb04b246df3a0a786b7d23ee563e8ad4493e8
Brian Barnes <houdinii1984@gmail.com>
2021-11-02 19:45:51 -0600

    lesson 2

--- app/src/main/java/com/houdinii/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/houdinii/coderswag/controller/MainActivity.kt
@@ -2,17 +2,27 @@ package com.houdinii.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.houdinii.coderswag.databinding.ActivityMainBinding
+import com.houdinii.coderswag.model.Category
+import com.houdinii.coderswag.services.DataService
 
 private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter : ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        // Don't forget to put binding. before elements now.
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+        binding.categoryListView.adapter = adapter
     }
 }
